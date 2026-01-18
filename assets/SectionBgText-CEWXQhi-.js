bcf295fae368c14bda42ece5ec950de9d0d32236
Karryl Dumalag <kjdumalag@gmail.com>
2026-01-18 11:02:39 -0800

    Updates

---
+++ assets/SectionBgText-CEWXQhi-.js
@@ -0,0 +1,7 @@
+import{p as e}from"./chunk-OIYGIGL5-Dh4DQa0S.js";function r({text:n}){return e.jsx("div",{className:"absolute inset-0 flex justify-start overflow-hidden pointer-events-none",children:e.jsx("h1",{className:`\r
+          text-[40vw]\r
+          text-gray-900/5 dark:text-white/5 \r
+          leading-none select-none \r
+          pl-[1vw] pb-[6vh]\r
+          whitespace-nowrap\r
+        `,children:n})})}export{r as S};
--- assets/extras-Vq_R53dE.js
+++
@@ -1,1 +0,0 @@
-import{w as t,p as r}from"./chunk-OIYGIGL5-Dh4DQa0S.js";const n=t(function(){return r.jsx("p",{children:"hi"})});export{n as default};
