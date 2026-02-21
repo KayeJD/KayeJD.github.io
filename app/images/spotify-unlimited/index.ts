78030f65781a4ad9d3c9bb7b3080d153644ee306
Karryl Dumalag <kjdumalag@gmail.com>
2026-02-20 19:24:10 -0800

    chore: Compress spotify main img

--- app/images/spotify-unlimited/index.ts
+++ app/images/spotify-unlimited/index.ts
@@ -1,7 +1,7 @@
 // import SpotifyCode1 from "./code1.png";
 // import SpotifyDashboard from "./dashboard.png";
 // import SpotifyLogin from "./login.png";
-import SpotifyMain from "./main.jpg";
+import SpotifyMain from "./spotifyMain.webp";
 
 export {
     // SpotifyCode1,
